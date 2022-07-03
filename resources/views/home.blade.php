a3c15611d3bbb612a6a417a480352b300e950483
Rifky Alamsyah <rifkyalamsyah30@gmail.com>
2022-07-03 19:59:20 +0700

    Add margin card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,23 +20,23 @@
                 </div> --}}
             </div>
             @foreach ($barangs as $barang)
-                <div class="col-md-4 ">
-                    <div class="card">
-                        <img src="{{ url('uploads') }}/{{ $barang->gambar }}" class="card-img-top"
-                            style="height: 20rem; object-fit: cover;" alt="barang">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $barang->nama_barang }}</h5>
-                            <p class="card-text">
-                                <strong>Harga :</strong> Rp. {{ number_format($barang->harga) }} <br>
-                                <strong>Stok :</strong> {{ $barang->stok }} <br>
-                                {{-- <strong>Keterangan :</strong> <br>
+                    <div class="col-md-4 g-4">
+                        <div class="card">
+                            <img src="{{ url('uploads') }}/{{ $barang->gambar }}" class="card-img-top"
+                                style="height: 20rem; object-fit: cover;" alt="barang">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $barang->nama_barang }}</h5>
+                                <p class="card-text">
+                                    <strong>Harga :</strong> Rp. {{ number_format($barang->harga) }} <br>
+                                    <strong>Stok :</strong> {{ $barang->stok }} <br>
+                                    {{-- <strong>Keterangan :</strong> <br>
                                 {{ $barang->keterangan }} --}}
-                            </p>
-                            <a href="{{ url('pesan') }}/{{ $barang->id }}" class="btn btn-primary"><i
-                                    class="fa fa-shopping-cart"></i> Pesan</a>
+                                </p>
+                                <a href="{{ url('pesan') }}/{{ $barang->id }}" class="btn btn-primary"><i
+                                        class="fa fa-shopping-cart"></i> Pesan</a>
+                            </div>
                         </div>
                     </div>
-                </div>
             @endforeach
         </div>
     </div>
